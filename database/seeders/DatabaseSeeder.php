a64c0584b6d7e0c6300fa0c792fb1a7973252153
dimasutamaa <dimasutamapa@gmail.com>
2024-11-17 22:23:19 +0700

    Product seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(CreateUsersSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
