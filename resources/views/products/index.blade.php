c16b614fa5bbefcc6c2d38bac1264bac9fcec887
dimasutamaa <dimasutamapa@gmail.com>
2024-11-19 16:39:40 +0700

    Replace product card markup with reusable Blade component

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -44,16 +44,7 @@
         <div class="row row-cols-1 row-cols-md-3 g-4">
             @forelse ($products as $product)
                 <div class="col">
-                    <a href="#" class="link-underline link-underline-opacity-0">
-                        <div class="bg-transparent border-0 card">
-                            <img src="{{ asset($product->product_image) }}" class="card-img-top rounded-4"
-                                alt="card example" />
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $product->title }}</h5>
-                                <p class="card-text">{{ 'Rp ' . number_format($product->price, 2, ',', '.') }}</p>
-                            </div>
-                        </div>
-                    </a>
+                    <x-product-card :product="$product" />
                 </div>
             @empty
                 <div class="mx-auto text-center text-secondary-emphasis">
